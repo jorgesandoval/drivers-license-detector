72fb62bd1096468a36fbb15108482d42abb4f4f3
Jorge G S Simao <jgssimao@gmail.com>
2022-11-18 16:51:11 -0300

    chore: create post method

--- resources/images.py
+++ resources/images.py
@@ -1,10 +1,37 @@
+import cv2
+import os
+from app import app
+from skimage.metrics import structural_similarity
+from PIL import Image
+from flask_restful import Resource 
 
+class ImageModification(Resource):
+    def resizeImage(self, file):
+        return Image.open(file).resize(250,160)
 
+    def transformGrayScale(self, image):
+        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    def calculateStructuralSimilarityDiff(self, file1, file2):
+        (score, diff) = structural_similarity(file1, file2, full=True)
+        diff =  (diff * 255).astype("uint8")
+        return diff
+
+    def calculateStructuralSimilarityScore(self, file1, file2):
+        (score, diff) = structural_similarity(file1, file2, full=True)
+        return score
+
+    def transformThreshold(self, diff):
+        return cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
 class Image(Resource):
 
-    def post(self, ):
-        return render_template('index.html',pred=str(round(score*100,2)) + '%' + ' correct')
+    def __init__(self, upload, original):
+        self.upload = 'model/uploads'
+        self.original = 'model/original'
 
-    def get(self):
-        return render_template("index.html")
+    def post(self, file):
+        originalImage = ImageModification.transformGrayScale(ImageModification.resizeImage(os.path.join(self.original,'driverslicencefront.png')))
+        uploadImage = ImageModification.transformGrayScale(ImageModification.resizeImage(os.path.join(self.upload,file)))
+        score = ImageModification.calculateStructuralSimilarityScore(self, originalImage, uploadImage)
+        return {'score': str(round(score*100,2)) + '%' + ' correct'}
