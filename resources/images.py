4fce90e528c4f9b556d447d0ab6b3b18b5f87240
Jorge G S Simao <jgssimao@gmail.com>
2022-11-18 22:24:25 -0300

    chore: rebase

--- resources/images.py
+++ resources/images.py
@@ -5,16 +5,10 @@ from flask_restful import Resource
 
 class ImageModification(Resource):
     def resizeImage(self, file):
-        #img = cv2.imread(file, cv2.IMREAD_UNCHANGED)
         return cv2.resize(cv2.imread(file, cv2.IMREAD_UNCHANGED), (250,150), interpolation = cv2.INTER_AREA)
 
     def transformGrayScale(self, img):
         return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    def calculateStructuralSimilarityDiff(self, img1, img2):
-        (score, diff) = structural_similarity(img1, img2, full=True)
-        diff =  (diff * 255).astype("uint8")
-        return diff
 
     def calculateStructuralSimilarityScore(self, file1, file2):
         (score, diff) = structural_similarity(file1, file2, full=True)
@@ -26,9 +20,9 @@ class ImageREST(Resource):
         upload = './models/upload/' 
         original = './models/original/'
         try:
-            originalImage = ImageModification.transformGrayScale(self,ImageModification.resizeImage(self, os.path.join(original,'DriversLicenseFront.png')))
+            originalImage = ImageModification.transformGrayScale(self,ImageModification.resizeImage(self, os.path.join(original,'DriversLicenseModel.png')))
             uploadImage = ImageModification.transformGrayScale(self,ImageModification.resizeImage(self, os.path.join(upload, file)))
             score = ImageModification.calculateStructuralSimilarityScore(self, originalImage, uploadImage)
             return {'score': str(round(score*100,2)) + '%' + ' correct'}, 200
         except:
-            return {'message': 'An error was found processing your image - {}'.format(os.path.join(upload,file))}, 500
+            return {'message': 'An error was found processing your image'}, 500
